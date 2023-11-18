73896221acc62285f301eb3ced734f9e7205dd0d
ksukrit <ksukrit2001@gmail.com>
2023-11-18 16:35:06 +0530

    Update testsets.py

--- ldm/data/testsets.py
+++ ldm/data/testsets.py
@@ -350,6 +350,7 @@ class Ucf101_triplet:
         self.gt_list = []
         for item in self.im_list:
             img_t = Image.open(join(db_dir, item , 'frame1.png'))
+            print(transforms.ToTensor()(img_t).shape)
             print(img_t.size)
             self.input3_list.append(self.transform(Image.open(join(db_dir, item , 'frame1.png'))).cuda().unsqueeze(0))
             self.input5_list.append(self.transform(Image.open(join(db_dir, item , 'frame2.png'))).cuda().unsqueeze(0))
